48fc2e86b0a860a99983d31b4892bbfa5f0faaf6
Uliana K. <krapivina.uo@gmail.com>
2025-10-30 21:37:08 +0500

    Add files via upload

---
+++ DataTypesExamples.java
@@ -0,0 +1,47 @@
+public class DataTypesExamples {
+    public static void main(String[] args) {
+        // ЦЕЛОЧИСЛЕННЫЕ ТИПЫ (byte, short, int, long)
+        
+        // byte 
+        byte byteValue = 100;
+        Byte byteObject = null;
+        System.out.println("byte: " + byteValue + ", Byte null: " + byteObject);
+        
+        // short
+        short shortValue = 30000;
+        Short shortObject = null;
+        System.out.println("short: " + shortValue + ", Short null: " + shortObject);
+        
+        // int 
+        int intValue = 2000000000;
+        Integer intObject = null;
+        System.out.println("int: " + intValue + ", Integer null: " + intObject);
+        
+        // long
+        long longValue = 9000000000000000000L;
+        Long longObject = null;
+        System.out.println("long: " + longValue + ", Long null: " + longObject);
+        
+        // ТИПЫ С ПЛАВАЮЩЕЙ ТОЧКОЙ (float, double)
+        
+        // float
+        float floatValue = 3.14f;
+        Float floatObject = null;
+        System.out.println("float: " + floatValue + ", Float null: " + floatObject);
+        
+        // double 
+        double doubleValue = 2.71828;
+        Double doubleObject = null;
+        System.out.println("double: " + doubleValue + ", Double null: " + doubleObject);
+        
+        // СИМВОЛЬНЫЙ ТИП (char)
+        char charValue = 'A';
+        Character charObject = null;
+        System.out.println("char: " + charValue + ", Character null: " + charObject);
+        
+        // БУЛЕВСКИЙ ТИП (bool) 
+        boolean booleanValue = true;
+        Boolean booleanObject = null;
+        System.out.println("boolean: " + booleanValue + ", Boolean null: " + booleanObject);
+    }
+}
