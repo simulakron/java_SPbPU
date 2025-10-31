b90fec8f94ffe9e1d77660549047cc3a1d60d734
Uliana K. <krapivina.uo@gmail.com>
2025-10-31 15:53:28 +0500

    Add files via upload

---
+++ Practice1.java
@@ -0,0 +1,15 @@
+public class Practice1 {
+    public static void main(String[] args) {
+        // Бесконечный цикл while
+        while (true) {  // условие всегда true - цикл никогда не завершится
+            System.out.println("Бесконечный цикл while");
+        }
+        
+        // Бесконечный цикл do-while (недостижимый код после while)
+        /*
+        do {
+            System.out.println("Бесконечный цикл do-while");
+        } while (true);  // условие всегда true - цикл никогда не завершится
+        */
+    }
+}
